7b4a451f624d380a6be0e2edecddda6b740002fc
desilva617 <diana_silva0617@outlook.com>
2023-11-05 02:33:56 -0500

    nuevos cambios

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # proyect_4
-Aplicación web
+Creación de una aplicación web para la visualización de un histograma y un gráfico de dispersión para el conjunto de datos de anunciós de coches.
+Tiene como función de facilitar las compras de los clientes a través de gráficos interactivos para que puedan elegir de acuerdo a sus intereses.
---
+++ app.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+        
+car_data = pd.read_csv('C:/Users/diana/Documents/mis proyectos/proyect_4/vehicles_us.csv')
+st.header('Características de los carros en anuncios de venta ')
+st.dataframe(car_data)
+
+hist_button = st.button('Construir histograma')
+scat_button = st.button('Construir un gráfico de dispersión')
+histo_button = st.button('Construir un histograma de la condición') # crear un botón
+        
+if hist_button: # al hacer clic en el botón
+            # escribir un mensaje
+    st.write('Creación de un histograma para el odómetro de coches en anuncio')
+            # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+            # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if scat_button:
+    st.write('Creación de un gráfico de dispersión relación odómetro - precio')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True) 
+
+if histo_button:
+    st.write('Creación de histograma de la condición de acuerdo al año del carro')
+    fig = px.histogram(car_data, x="model_year", color = 'condition')
+    st.plotly_chart(fig, use_container_width=True) 
